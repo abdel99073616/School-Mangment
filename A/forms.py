da8e4500b874c60dbd43fd48600ceda3ce9e561d
Abdo <you@example.com>
2020-10-20 01:28:09 +0200

    return

--- A/forms.py
+++ A/forms.py
@@ -26,25 +26,3 @@ class RowProdectForm(forms.Form):
     last_login = forms.DateTimeField()
 
 
-
-class Teacher_AForm(forms.ModelForm):
-    class Meta:
-        model = Teacher_A
-        fields =[
-            'Fname' ,
-            'Lname' ,
-            'address' ,
-            'Date_of_Bath' ,
-            'password' ,
-            'Email' ,
-            'last_login',
-        ]
-   
-class RowProdectForm(forms.Form):
-    Fname = forms.CharField()
-    Lname = forms.CharField()
-    address = forms.CharField()
-    Date_of_Bath = forms.DateField()
-    password = forms.CharField()
-    Email = forms.EmailField()
-    last_login = forms.DateTimeField()
