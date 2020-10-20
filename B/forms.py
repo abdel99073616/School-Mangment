501a7141de503f763b1910cc68efdb9cd67979a0
Abdo Abosamra <abdo.abosamra80@gmail.com>
2020-10-20 02:19:48 +0200

    Frist Form

--- B/forms.py
+++ B/forms.py
@@ -1,1 +1,27 @@
 from django import forms 
+
+
+
+from .models import  Teacher_B
+
+class Teacher_BForm(forms.ModelForm):
+    class Meta:
+        model = Teacher_B
+        fields =[
+            'Fname' ,
+            'Lname' ,
+            'address' ,
+            'Date_of_Bath' ,
+            'password' ,
+            'Email' ,
+            'last_login',
+        ]
+   
+class RowProdectForm(forms.Form):
+    Fname = forms.CharField()
+    Lname = forms.CharField()
+    address = forms.CharField()
+    Date_of_Bath = forms.DateField()
+    password = forms.CharField()
+    Email = forms.EmailField()
+    last_login = forms.DateTimeField()
